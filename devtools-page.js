65aceda8c2a1ac12e71d4780dc5a2982e09d4d1f
Luis Ramos <lmramos89@gmail.com>
2025-06-07 10:18:23 -0400

    Revert "Initial Push"
    This reverts commit f167d6ac9afb59733334b6efc6d525fc8849fb08.

--- devtools-page.js
+++
@@ -1,9 +0,0 @@
-/**
- * Photonic DevTools Page Script
- * This script runs in the devtools page context and creates the panel
- */
-
-// Create the DevTools panel
-chrome.devtools.panels.create('Photonic', '', 'devtools.html', () => {
-  console.log('Photonic DevTools panel created');
-});
